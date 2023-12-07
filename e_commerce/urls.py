635c631183582be46f5928ab744e927f01cb4eb7
shijil <shijilks2000@gmail.com>
2023-12-07 13:36:44 +0530

    first commit

---
+++ e_commerce/urls.py
@@ -0,0 +1,11 @@
+
+from django.urls import path 
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+     path('', views.home),
+     path('about/', views.about,name="about"),
+     path('contact/', views.contact,name="contact"),
+     path('category/<slug:val>', views.CategoryView.as_view(), name="category"),
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
---
+++ e_commerce/views.py
@@ -0,0 +1,20 @@
+from django.db.models import Count
+from django.shortcuts import render
+from django.views import View
+from .models import Product
+
+# Create your views here.
+def home(request):
+    return render(request,'home.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+class CategoryView(View):
+    def get(self,request,val):
+        product = Product.objects.filter(category=val)
+        title = Product.objects.filter(category=val).values('title')
+        return render(request,'category.html',locals())
